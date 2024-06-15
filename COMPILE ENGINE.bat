686fe1716ba7dc6b88071ef5fe5bbf77e1584f61
PC <156932876+bonnieplay873@users.noreply.github.com>
2024-06-15 17:20:44 +0300

    debug

--- COMPILE ENGINE.bat
+++ COMPILE ENGINE.bat
@@ -15,23 +15,3 @@ if /i "%debug%"=="y" (
     pause
     exit /b 1
 )
-
-set /p build="On Build Mode? (y/n): "
-
-if /i "%build%"=="y" (
-    if defined debug_flag (
-        lime build windows %debug_flag%
-    ) else (
-        lime build windows
-    )
-) else if /i "%build%"=="n" (
-    if defined debug_flag (
-        lime test windows %debug_flag%
-    ) else (
-        lime test windows
-    )
-) else (
-    echo Invalid input. Please enter y or n.
-    pause
-    exit /b 1
-)
